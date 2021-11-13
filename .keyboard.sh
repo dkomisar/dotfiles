d712e17d476f8df06e173b1e949f686bd12b099e
Daniel Komisar <dkomisar@gmail.com>
2021-11-13 10:56:56 -0500

    reorder keyboard

--- .keyboard.sh
+++ .keyboard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# send ESC on CTRL release if nothing else pressed
-xcape -e 'Control_L=Escape'
-
 # load xmodmap settings (swap CAPS/CTRL_L)
 # note `setxkbmap -option` will reset meta keys
 xmodmap ~/.xmodmap
+
+# send ESC on CTRL release if nothing else pressed
+xcape -e 'Control_L=Escape'
 
